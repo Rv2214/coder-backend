5c7fa4c7457dd2964f7e2ebc151daf96a0ca55e1
Rv2214 <rodrigo.e.vazquez@gmail.com>
2023-12-26 21:55:38 -0300

    desafio2_terminado

--- 01_desafio2_fs/product-manager.js
+++ 01_desafio2_fs/product-manager.js
@@ -1,55 +1,64 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 class productManager {
   static #perGain = 0.3;
   static #totalGain = 0;
+  static #products = [];
 
   constructor(path) {
     this.path = path;
-    this.products = [];
     this.init();
   }
+
   init() {
-    const file = fs.existsSync(this.path);
-    if (file) {
-      this.products = JSON.parse(fs.readFileSync(this.path, "utf-8"));
-    } else {
-      fs.writeFileSync(this.path, JSON.stringify([], null, 2));
+    try {
+      const file = fs.existsSync(this.path);
+      if (!file) {
+        fs.writeFileSync(this.path, JSON.stringify([], null, 2));
+      } else {
+        const fileContent = fs.readFileSync(this.path, "utf-8");
+        if (fileContent.trim() === "") {
+          // Handle empty file
+          productManager.#products = [];
+        } else {
+          productManager.#products = JSON.parse(fileContent);
+        }
+      }
+    } catch (error) {
+      console.error("Error initializing productManager:", error.message);
     }
   }
-  async createProduct({ title, description, ...data }) {
+
+  async create(data) {
     try {
-      if (!title || !description) {
-        throw new Error("Inserte titulo y descripcion");
+      if (!data.title || !data.price || !data.stock) {
+        throw new Error("Inserte titulo, precio y stock");
+      } else {
+        const product = {
+          id: crypto.randomBytes(12).toString("hex"),
+          title: data.title,
+          photo: "sin imagen",
+          price: data.price,
+          stock: data.stock,
+        };
+        productManager.#products.push(product);
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(productManager.#products, null, 2)
+        );
+        return true;
       }
-      const product = {
-        id:
-          this.products.length === 0
-            ? 1
-            : this.products[this.products.length - 1].id + 1,
-        title,
-        description,
-        photo: "sin imagen",
-        price: data.price,
-        stock: data.stock,
-      };
-      this.products.push(product);
-      //console.log("this.products:", this.products);
-      await fs.promises.writeFile(
-        this.path,
-        JSON.stringify(this.products, null, 2)
-      );
-      return true;
     } catch (error) {
       return error.message;
     }
   }
   readProducts() {
     try {
-      if (this.products.length === 0) {
+      if (productManager.#products.length === 0) {
         throw new Error("No se encontraron productos");
       } else {
-        return this.products;
+        return productManager.#products;
       }
     } catch (error) {
       return error.message;
@@ -57,7 +66,9 @@ class productManager {
   }
   readOne(id) {
     try {
-      const productId = this.products.find((each) => each.id === Number(id));
+      const productId = productManager.#products.find(
+        (each) => each.id === String(id)
+      );
       if (!productId) {
         throw new Error("no se encontro el producto");
       } else {
@@ -80,9 +91,10 @@ class productManager {
           productManager.#totalGain +
           quantity * product.price * productManager.#perGain;
         await fs.promises.writeFile(
-          this.path.JSON.stringify(this.products, null, 2)
+          this.path,
+          JSON.stringify(productManager.#products, null, 2)
         );
-        return true;
+        return { stock: product.stock, totalGain: productManager.#totalGain };
       }
     } catch (error) {
       return error.message;
@@ -93,19 +105,19 @@ class productManager {
   }
   async deleteProductById(id) {
     try {
-      const index = this.products.findIndex(
-        (product) => product.id === Number(id)
+      const index = productManager.#products.findIndex(
+        (product) => product.id === id
       );
 
       if (index === -1) {
         throw new Error("No se encontró el producto con el ID proporcionado");
       }
 
-      this.products.splice(index, 1);
+      productManager.#products.splice(index, 1);
 
       await fs.promises.writeFile(
         this.path,
-        JSON.stringify(this.products, null, 2)
+        JSON.stringify(productManager.#products, null, 2)
       );
 
       return true;
@@ -115,19 +127,19 @@ class productManager {
   }
   async updateProduct(id, newData) {
     try {
-      const index = this.products.findIndex(
-        (product) => product.id === Number(id)
+      const index = productManager.#products.findIndex(
+        (product) => product.id === id
       );
       if (index === -1) {
         throw new Error("No se encontro el producto con el ID proporcionado");
       }
 
-      const updatedProduct = { ...this.products[index], ...newData };
-      this.products[index] = updatedProduct;
+      const updatedProduct = { ...productManager.#products[index], ...newData };
+      productManager.#products[index] = updatedProduct;
 
       await fs.promises.writeFile(
         this.path,
-        JSON.stringify(this.products, null, 2)
+        JSON.stringify(productManager.#products, null, 2)
       );
       return updatedProduct;
     } catch (error) {
@@ -138,64 +150,75 @@ class productManager {
 
 const product = new productManager("./01_desafio2_fs/app/data/products.json");
 
-product
-  .createProduct({
-    /*     title: "Nuevo Producto", 
+product.create({
+  /*     title: "Nuevo Producto",
     price: 19.99,
-    stock: 50 */
-  })
-  .then((newProduct) => {
+    stock: 50, */
+});
+/*   .then((newProduct) => {
     console.log(newProduct);
   })
   .catch((error) => {
     console.error(error);
   });
 
-// Probar método eliminar
-const productIdToDelete = 13; // id a eliminar
-const productToDelete = product.readOne(productIdToDelete);
-
-if (productToDelete) {
-  product
-    .deleteProductById(productIdToDelete)
-    .then((result) => {
-      if (result) {
-        console.log("Producto eliminado correctamente");
-      } else {
-        console.log(
-          "Error: No se encontró el producto con el ID proporcionado"
-        );
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-} else {
-  console.log("Error: No se encontró el producto con el ID proporcionado");
+//leer producto
+try {
+  const products = product.readProducts();
+  console.log("Productos:", products);
+} catch (error) {
+  console.error("Error al leer los productos:", error);
 }
 
-const productIdToUpdate = 13; // ID del producto a actualizar
-const newData = {
-  title: "Nuevo Título",
-  price: 29.99,
-  stock: 20,
-};
+//leer producto por id
+const productIdToRead = "c6ab3c36cd07447e7dde28e2";
+try {
+  const productById = product.readOne(productIdToRead);
+  console.log("Producto por ID:", productById);
+} catch (error) {
+  console.error("Error al leer el producto por ID:", error);
+}
 
+//vender producto
+const productIdToSell = "c6ab3c36cd07447e7dde28e2";
+const quantityToSell = 5;
+product
+  .soldProduct(quantityToSell, productIdToSell)
+  .then(({ stock, totalGain }) => {
+    console.log(
+      `Producto vendido. Stock restante: ${stock}. Ganancia total: ${totalGain}`
+    );
+  })
+  .catch((error) => {
+    console.error("Error al vender el producto:", error);
+  });
+ */
+
+//eliminar producto por id
+
+/* const productIdToDelete = "3be2748e60c419e9d768c74c"; // Reemplazar con un ID real
+product
+  .deleteProductById(productIdToDelete)
+  .then((result) => {
+    console.log("Producto eliminado");
+  })
+  .catch((error) => {
+    console.error("Error al eliminar el producto:", error);
+  });
+ */
+
+/* // actualizar producto
+const productIdToUpdate = "c6ab3c36cd07447e7dde28e2"; // Reemplazar con un ID real
+const newData = {
+  title: "Cebollas",
+  price: 30,
+  stock: 50,
+};
 product
   .updateProduct(productIdToUpdate, newData)
   .then((updatedProduct) => {
-    if (updatedProduct) {
-      console.log("Producto actualizado correctamente:", updatedProduct);
-    } else {
-      console.log("Error: No se encontró el producto con el ID proporcionado");
-    }
+    console.log("Producto actualizado:", updatedProduct);
   })
   .catch((error) => {
-    console.error(error);
-  });
-
-/* product.soldProduct(10, 5);
-
-console.log(product.getGain());
-console.log(product.readOne(5));
-//console.log(product.readProducts()); */
+    console.error("Error al actualizar el producto:", error);
+  }); */
